f9df816136133e3f9739594bd681f21d92b35f37
Anton Demkin <avdemkin@avito.ru>
2023-04-08 19:50:04 +0400

    Translate buttons to Russian

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ createApp({
     <div v-if="question">
       <h1>{{ question.text }}</h1>
       <p>баллы: {{ score }}</p>
-      <button class="yes" @click="handleAnswer(question.yes)">Yes</button>
-      <button class="no" @click="handleAnswer(question.no)">No</button>
+      <button class="yes" @click="handleAnswer(question.yes)">Да</button>
+      <button class="no" @click="handleAnswer(question.no)">Нет</button>
     </div>
     <div v-else>
       <h1>Результат:</h1>
